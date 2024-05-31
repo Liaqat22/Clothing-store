babb086da7fa88edd611c8c367f009f2dbea3e15
Liaqat Ali <126480140+Liaqat22@users.noreply.github.com>
2024-05-31 16:02:14 +0500

    Update Footer.js

--- Frontend/src/component/Layout/Footer.js
+++ Frontend/src/component/Layout/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
     <div>
       
       <div className='bg-dark p-2' >
-        <h3 className='text-center p-3 text-white footerText'>Copyright &copy; by Liaqat Ali 2024. All Rights are Reserved</h3>
+        <h3 className='text-center p-3 text-white footerText'>Copyright &copy; by Ali Haider 2024. All Rights are Reserved</h3>
       </div>
 
     </div>
